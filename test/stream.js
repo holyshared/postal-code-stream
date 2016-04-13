c224518b2fad320beebf21d3709a3abd2c986ab1
holyshared <holy.shared.design@gmail.com>
2016-04-13 10:10:33 +0900

    add csv stream

--- test/stream.js
+++ test/stream.js
@@ -12,4 +12,15 @@ describe('stream', () => {
       assert.ok(this.results.length === 100);
     });
   });
+  describe('#createFromCSVFile', () => {
+    beforeEach(function(done) {
+      this.results = [];
+      this.stream = stream.createFromCSVFile('test/fixtures/dump.csv');
+      this.stream.on('data', (r) => this.results.push(r));
+      this.stream.on('end', done);
+    });
+    it('should be read from csv file', function() {
+      assert.ok(this.results.length === 100);
+    });
+  });
 });
